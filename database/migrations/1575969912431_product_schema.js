f503fbc3a5d3c62c4f5353623110b7e308f5c077
Pedro Fuzario <pedrofuzario@hotmail.com>
2019-12-10 10:54:21 -0400

    product and coupon schema

--- database/migrations/1575969912431_product_schema.js
+++ database/migrations/1575969912431_product_schema.js
@@ -16,16 +16,25 @@ class ProductSchema extends Schema {
       table.foreign('image_id').references('id').inTable('images').onDelete('cascade')
     })
 
-    this.create('image_product', table => {
+    this.create('image_product', (table) => {
       table.increments()
       table.integer('image_id').unsigned()
       table.integer('product_id').unsigned()
       table.foreign('image_id').references('id').inTable('images').onDelete('cascade')
       table.foreign('product_id').references('id').inTable('products').onDelete('cascade')
     })
+
+    this.create('category_product', (table) =>{
+      table.increments()
+      table.integer('product_id').unsigned()
+      table.integer('category_id').unsigned()
+      table.foreign('product_id').references('id').inTable('products').onDelete('cascade')
+      table.foreign('category_id').references('id').inTable('categories').onDelete('cascade')
+    })
   }
 
   down () {
+    this.drop('category_product')
     this.drop('image_product')
     this.drop('products')
   }
