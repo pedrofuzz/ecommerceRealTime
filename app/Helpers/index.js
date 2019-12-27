64f2756c71f3a1b31467fa98a7cc3ecd957b8663
Pedro Fuzario <pedrofuzario@hotmail.com>
2019-12-27 13:20:00 -0400

    Updated Buffer for new version

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -17,8 +17,8 @@ const Helpers = use('Helpers')
      if(len < length){
          let size = length - len
          let bytes = await crypto.randomBytes(size)
-         let buffer = new Buffer(bytes)
-         string += buffer.toString('base64').replace(/[ˆa-zA-Z0-9]/g, '').substr(0, size)
+         let buffer = Buffer.from(bytes)
+         string += buffer.toString('base64').replace(/[^a-zA-Z0-9]/g, '').substr(0, size)
      }
 
      return string
